8dbc2597aa6adba316082232b28fc1903951ad23
Murdo Maclachlan <murdo@maclachlans.org.uk>
2021-03-05 19:32:47 +0000

    Create __init__.py

---
+++ utorsmodules/__init__.py
@@ -0,0 +1,33 @@
+# .args
+from .args import checkArgs
+from .args import program
+from .args import checkClaim
+from .args import getPeriod
+from .args import graph
+
+# .funcs
+from .funcs import countTranscriptions
+from .funcs import checkTranscriptions
+from .funcs import checkTranscribers
+from .funcs import coverage
+
+# .graph
+from .graph import graph
+from .graph import barChart
+from .graph import pieChart
+from .graph import makeTitle
+
+# .misc
+from .misc import bubbleSort
+from .misc import fetchPosts
+from .misc import findAuthor
+from .misc import makeSecondaryLists
+from .misc import removeNonAlpha
+from .misc import removePost
+from .misc import satisfiesRequirements
+from .misc import strToBool
+from .misc import timeCheck
+from .misc import valid
+
+# the program
+from .main import everything
